f33bdc77bb518143359ea84464fa9457349b4d3f
Neel chodvadiya <cneel111@gmail.com>
2023-06-01 18:51:06 +0530

    category added

---
+++ Cooking_Hub/Controllers/Admin/AdminCategoriesController.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Cooking_Hub.Models;
+
+namespace Cooking_Hub.Controllers.Admin
+{
+    public class AdminCategoriesController : Controller
+    {
+        private readonly CookingHubContext _context;
+        private readonly IWebHostEnvironment hostEnvironment;
+
+        public AdminCategoriesController(CookingHubContext context, IWebHostEnvironment hostEnvironment)
+        {
+            _context = context;
+            this.hostEnvironment = hostEnvironment;
+        }
+
+        // GET: AdminCategories
+        public async Task<IActionResult> Index()
+        {
+              return _context.Categories != null ? 
+                          View(await _context.Categories.ToListAsync()) :
+                          Problem("Entity set 'CookingHubContext.Categories'  is null.");
+        }
+
+        // GET: AdminCategories/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null || _context.Categories == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories
+                .FirstOrDefaultAsync(m => m.CategoryId == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // GET: AdminCategories/Create
+        public IActionResult Create()
+        {
+            string categoryId = Guid.NewGuid().ToString();
+            var category = new Category { CategoryId = categoryId };
+            return View(category);
+        }
+
+        // POST: AdminCategories/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CategoryId,CategoryName,CategoryIsActive,CategoryImage")] Category category, IFormFile photo)
+        {
+            if (ModelState.IsValid)
+            {
+             
+                if (photo != null)
+                {
+                    string filename = photo.FileName;
+                    string filepath = Path.Combine(hostEnvironment.WebRootPath, "UserImage", filename);
+
+                    using (var stream = new FileStream(filepath, FileMode.Create))
+                    {
+
+                        await photo.CopyToAsync(stream);
+                    }
+                    category.CategoryImage = filename;
+
+                }
+                _context.Add(category);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        // GET: AdminCategories/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.Categories == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        // POST: AdminCategories/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("CategoryId,CategoryName,CategoryIsActive,CategoryImage")] Category category)
+        {
+            if (id != category.CategoryId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(category);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CategoryExists(category.CategoryId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        // GET: AdminCategories/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.Categories == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories
+                .FirstOrDefaultAsync(m => m.CategoryId == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // POST: AdminCategories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.Categories == null)
+            {
+                return Problem("Entity set 'CookingHubContext.Categories'  is null.");
+            }
+            var category = await _context.Categories.FindAsync(id);
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CategoryExists(string id)
+        {
+          return (_context.Categories?.Any(e => e.CategoryId == id)).GetValueOrDefault();
+        }
+    }
+}
